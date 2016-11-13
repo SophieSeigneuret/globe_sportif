eed22063fb2cf997bbd281bb89a5f8baff19700c
Sophie Seigneuret <sophieseigneuret@MBPdeSoigneuret.local>
2016-11-12 23:01:20 -0500

    mise à jour des données du fichier forfaits.js

--- script/main.js
+++ script/main.js
@@ -7,3 +7,34 @@
 $( function() {
     $( "#onglets" ).tabs();
 } );
+
+
+// AJOUT DESCRIPTIF PAGE DETAIL
+var forfait_voulu = rechercher_descriptif_forfait("forfait_8");
+// nom categorie
+document.querySelector("#info_forfait h2").textContent = forfait_voulu.categorie.toUpperCase();
+// nom forfait
+document.querySelector("#descriptif h3").innerHTML = forfait_voulu.nom;
+// date de début de saison
+document.querySelector("#descriptif p:first-of-type span:first-of-type").innerHTML = forfait_voulu.debut_saison;
+// date de fin de saison
+document.querySelector("#descriptif p:first-of-type span:last-of-type").innerHTML = forfait_voulu.fin_saison;
+// durée du séjour
+document.querySelector("#descriptif p:nth-of-type(2) span").innerHTML = forfait_voulu.duree;
+// animaux autorisés
+document.querySelector("#descriptif p:nth-of-type(3) span").innerHTML = forfait_voulu.nbr_max_animaux_admis;
+// places restantes
+document.querySelector("#descriptif p:nth-of-type(4) span").innerHTML = forfait_voulu.places_dispo;
+// prix par personne
+document.querySelector("#descriptif h4 span").innerHTML = forfait_voulu.prix;
+
+// AJOUT DÉTAILS ONGLETS
+// le lieu
+document.querySelector("#le_lieu p").innerHTML = forfait_voulu.lieu;
+// les infos pratiques
+document.querySelector("#infos_pratiques p").innerHTML = forfait_voulu.infos;
+// l'hébergement
+document.querySelector("#hebergement p").innerHTML = forfait_voulu.hebergement;
+// niveau
+document.querySelector("#niveau p").innerHTML = forfait_voulu.niveau;
+
